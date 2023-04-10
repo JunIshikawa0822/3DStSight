c95aa9a1305e8b8f572fb158c108a63faf441a30
JunIshikawa0822 <jun.ffcc83@gmail.com>
2023-04-10 13:41:44 +0900

    Player Fundamental Movement Done

---
+++ Assets/0_Core/0_Scripts/CamerMoveScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamerMoveScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    GameObject Player;
+    Camera MainCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GameObject.Find("Player");
+        MainCamera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MainCamera.gameObject.transform.position = Player.transform.position + new Vector3(0, 0.4f, -1.2f);
+    }
+}
---
+++ Assets/0_Core/0_Scripts/PlayerMoveScript.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoveScript : MonoBehaviour
+{
+    //[SerializeField]
+    GameObject Player;
+
+    // 初速度
+    private float _playerInitialVelocity;
+
+    // 現在速度
+    private float _playerVelocity;
+
+    Vector3 inputDirection;
+
+    Animator playerAnimator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GameObject.Find("Player");
+        _playerVelocity = 0.005f;
+        inputDirection = new Vector3(1, 0, 0);
+        playerAnimator = Player.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMove();
+    }
+
+    public void PlayerMove()
+    {
+        inputDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        Player.transform.position += inputDirection * _playerVelocity;
+
+        playerAnimator.SetFloat("Player.x", inputDirection.x);
+        playerAnimator.SetFloat("Player.y", inputDirection.z);
+
+    }
+}
