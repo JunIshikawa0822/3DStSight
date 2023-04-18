74ed5fb38d4813bb01e1faf7e06c867c96ae638b
JunIshikawa0822 <jun.ffcc83@gmail.com>
2023-04-18 09:42:34 +0900

    cameraの挙動を作成開始

--- Assets/0_Core/0_Scripts/CamerMoveScript.cs
+++ Assets/0_Core/0_Scripts/CamerMoveScript.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class CamerMoveScript : MonoBehaviour
 {
     // Start is called before the first frame update
     GameObject Player;
+    GameObject MouseObject;
     Camera MainCamera;
     // Start is called before the first frame update
     void Start()
@@ -18,5 +20,11 @@ public class CamerMoveScript : MonoBehaviour
     void Update()
     {
         MainCamera.gameObject.transform.position = Player.transform.position + new Vector3(0, 0.6f, -2f);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 CameraMove = new Vector3(MouseObject.transform.position.x, 0.6f, MouseObject.transform.position.z) * 0.6f;
+            transform.DOLocalMove((CameraMove), 1f).SetRelative(true);
+        }
     }
 }
